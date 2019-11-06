28cf99c26b142358f386d312625f29ccdb81e8d9
Radoslav Georgiev <rgeorgiev583@gmail.com>
2019-11-06 23:13:11 +0200

    add solutions to exercises from the fifth meeting

---
+++ 5/1-read-redirect.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define REQUIRED_ARG_COUNT 1
+
+int main(int argc, const char *const *argv)
+{
+    if (argc < REQUIRED_ARG_COUNT + 1)
+        exit(EXIT_FAILURE);
+
+    int input_fileno = open(argv[1], O_RDONLY);
+    if (-1 == input_fileno)
+    {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+
+    if (-1 == dup2(input_fileno, STDIN_FILENO))
+    {
+        perror("dup2");
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[BUFSIZ];
+    ssize_t read_count;
+    while ((read_count = read(STDIN_FILENO, buffer, BUFSIZ)) != 0)
+    {
+        if (-1 == read_count)
+        {
+            perror("read");
+            exit(EXIT_FAILURE);
+        }
+
+        if (-1 == write(STDOUT_FILENO, &buffer, read_count))
+        {
+            perror("write");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return 0;
+}
---
+++ 5/2-write-redirect.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define DEFAULT_FILE_MODE S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
+
+#define REQUIRED_ARG_COUNT 1
+
+int main(int argc, const char *const *argv)
+{
+    if (argc < REQUIRED_ARG_COUNT + 1)
+        exit(EXIT_FAILURE);
+
+    int output_fileno = creat(argv[1], DEFAULT_FILE_MODE);
+    if (-1 == output_fileno)
+    {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+
+    if (-1 == dup2(output_fileno, STDOUT_FILENO))
+    {
+        perror("dup2");
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[BUFSIZ];
+    ssize_t read_count;
+    while ((read_count = read(STDIN_FILENO, buffer, BUFSIZ)) != 0)
+    {
+        if (-1 == read_count)
+        {
+            perror("read");
+            exit(EXIT_FAILURE);
+        }
+
+        if (-1 == write(STDOUT_FILENO, &buffer, read_count))
+        {
+            perror("write");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return 0;
+}
